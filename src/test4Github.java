493f0c845d9fb7db31601f442f70abf463170810
BY3D <49314704+BY3D@users.noreply.github.com>
2025-03-19 15:57:29 -0400

    Trying things out

---
+++ src/test4Github.java
@@ -0,0 +1,14 @@
+// For experimenting with Git & Github since they're finicky
+
+public class test4Github {
+    private int hi;
+
+    test4Github(int hi) {
+        this.hi = hi;
+    }
+
+    public void print() {
+        System.out.println(hi);
+    }
+
+}
